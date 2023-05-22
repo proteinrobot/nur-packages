2b238b9faef807c4687c14fcd25f7d03fd580711
proteinrobot <proteinrobot@pm.me>
2023-05-22 17:37:45 +0800

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "My personal NUR repository";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (system: import ./default.nix {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    };
+}
